80a5b8fceedf3e9080a8a905e0af575c5b83f32a
sayaxxx <yetissohara@gmail.com>
2023-09-06 19:55:06 -0500

    :white_check_mark: New functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,71 @@
-document.getElementById("nameForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const name = document.getElementById("nameInput").value;
-    // Redirige a la página de preguntas
-    window.location.href = "test.html"; // Reemplaza "preguntas.html" con la URL de tu página de preguntas
+// Obtener todas las preguntas
+const questions = document.querySelectorAll(".question");
+
+// Contador de puntos
+let puntos = 0;
+
+// Función para mostrar la siguiente pregunta
+function showNextQuestion(currentQuestionIndex) {
+    if (currentQuestionIndex < questions.length) {
+        questions[currentQuestionIndex].style.display = "block";
+    }
+}
+
+// Función para calcular los puntos
+function calcularPuntos() {
+    // Obtener todas las respuestas seleccionadas en las preguntas correspondientes
+    const selectedAnswers = [];
+    questions.forEach((question, index) => {
+        if ([0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14, 15, 18, 19].includes(index)) {
+            const radioButton = question.querySelector('input[type="radio"]:checked');
+            if (radioButton && radioButton.value === "Si") {
+                puntos += 5;
+            }
+        }
+        else if([9, 16, 17].includes(index)) {
+            const radioButton = question.querySelector('input[type="radio"]:checked');
+            if (radioButton && radioButton.value === "No") {
+                puntos += 5;
+            }
+        }
+        else if([8].includes(index)) {
+            const radioButton = question.querySelector('input[type="radio"]:checked');
+            if (radioButton && radioButton.value === "NoEsCierta"){
+                puntos += 5;
+            }
+        }
+        else if([13].includes(index)){
+            const radioButton = question.querySelector('input[type="radio"]:checked');
+            if (radioButton && radioButton.value === "SiLoSe"){
+                puntos += 5;
+            }
+        }
+    });
+
+    // Actualizar el contador de puntos en la página
+    const puntosContainer = document.getElementById("puntos");
+    if (puntosContainer) {
+        puntosContainer.textContent = `Puntos: ${puntos}`;
+    }
+}
+
+// Escuchar cambios en las respuestas
+questions.forEach((question, index) => {
+    const radioButtons = question.querySelectorAll('input[type="radio"]');
+
+    radioButtons.forEach(radioButton => {
+        radioButton.addEventListener("change", () => {
+            // Ocultar la pregunta actual
+            question.style.display = "none";
+
+            // Mostrar la siguiente pregunta
+            showNextQuestion(index + 1);
+
+            // Calcular los puntos al responder
+            calcularPuntos();
+        });
+    });
 });
+
+// Mostrar la primera pregunta al cargar la página
+showNextQuestion(0);
