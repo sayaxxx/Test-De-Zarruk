d8bc30a247f899204aadc23923ef1268d075bf10
sayaxxx <yetissohara@gmail.com>
2023-09-08 16:49:09 -0500

    fix: :bug: Change of route

--- js/indexjs.js
+++ js/indexjs.js
@@ -2,5 +2,5 @@ document.getElementById("nameForm").addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("nameInput").value;
     // Redirige a la página de preguntas
-    window.location.href = "/test.html"; 
+    window.location.href = "./test.html"; 
 });
