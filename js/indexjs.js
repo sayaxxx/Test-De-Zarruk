14893634b3b42e73806c0d895903f230d8300e4c
sayaxxx <yetissohara@gmail.com>
2023-09-12 01:14:57 -0500

    fix: :bug: Error

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,6 +1,6 @@
 document.getElementById("nameForm").addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("nameInput").value;
-    e.currentTarget.action = "./test.html";
+    e.currentTarget.action = "../test.html";
     e.currentTarget.submit();
 });
