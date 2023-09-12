3eca06436579abee444a044254f34c53b71a5bb1
sayaxxx <yetissohara@gmail.com>
2023-09-12 01:08:00 -0500

    feat: :sparkles: API connection

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,6 +1,6 @@
 document.getElementById("nameForm").addEventListener("submit", function (e) {
     e.preventDefault();
     const name = document.getElementById("nameInput").value;
-    // Redirige a la página de preguntas
-    window.location.href = "./test.html"; 
+    e.currentTarget.action = "./test.html";
+    e.currentTarget.submit();
 });
