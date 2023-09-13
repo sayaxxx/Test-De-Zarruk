b5e9ecfd6ad35dd9fae85f99cf64a96131616e8b
sayaxxx <yetissohara@gmail.com>
2023-09-13 18:37:21 -0500

    feat: :sparkles: Validate hidden input

--- js/indexjs.js
+++ js/indexjs.js
@@ -2,3 +2,12 @@ document.getElementById("nameForm").addEventListener("submit", function (e) {
     const name = document.getElementById("nameInput").value;
 });
 
+document.getElementById("secretCode").addEventListener("change", function(e) {
+    const secretValue = e.target.value;
+    const correctValue = 1234;
+
+    if (secretValue == correctValue) {
+        window.location.href = "./sorteo.html";
+    }
+});
+
